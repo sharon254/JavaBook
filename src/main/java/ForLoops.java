cf62395e00934c5d9e36460791779bf0845c8bef
sharon254 <sharonknyabuti@gmail.com>
2023-01-09 21:59:50 +0300

    Loops

---
+++ src/main/java/ForLoops.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class ForLoops {
+    public static void main(String[]args){
+
+        /**
+         * for loop when you know how many times you need the code to run
+         *use while loop when its not clear how many times the code need to run, has no counter
+         * For loop
+         *1.start
+         * 2.stop
+         * 3.step size
+         *
+         */
+//        for(int i =1; i<=5;i++){
+//            System.out.println(i + ". I will not forget to write semicolon");
+//        };
+//        for(int i = 99; i >91; i--){
+//            System.out.println(i +" bottles of beer on the wall " + i + " bottles on the wall");
+//        }
+//        System.out.print("What range do you want to print your numbers?");
+//        int userInPut = scan.nextInt();
+//        System.out.println("This is a number between 1 and " + userInPut);
+//        System.out.println("Great this os how it is done: ");
+////        for(int i = 1; i <= userInPut; i++){
+////           System.out.print( " "+ i + " " );
+////        };
+        //System.out.println("Enter Number");
+        Scanner scan = new Scanner(System.in);
+//        int valueNumber = scan.nextInt();
+////        int valueNumber = 10;
+//        if (valueNumber % 2 == 0){
+//            System.out.println("Number is even");
+//        }
+//        else {
+//            System.out.println("Number is odd");
+//
+//        }
+        System.out.println("Print lines from");
+        int numberComparison = scan.nextInt();
+        for (int i = 1;i<=numberComparison;i++){
+            if ( i % 2 == 0){
+                System.out.println(i + ": even");
+            }
+            else {
+                System.out.println(i + ": " +
+                        "odd");
+
+            }
+        }
+
+
+//        System.out.println("I hear you like to count by threes");
+//        System.out.println("Pick a number to count by:");
+//        int countBy = scan.nextInt();
+//        System.out.println(countBy);
+//        System.out.println("Pick a number to start counting from:");
+//        int countFrom = scan.nextInt();
+//        System.out.println(countFrom);
+//        System.out.println("Pick a number to count to");
+//        int countTo = scan.nextInt();
+//        System.out.println(countTo);
+//        for (int i =countFrom;i<=countTo;i=i +countBy){
+//            System.out.println(i);
+//        }
+
+        /**
+         * i++ is the same as i+=1
+         * i= i + 1
+         */
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,18 +2,13 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[]args){
-
         System.out.println("Do you want to play?");
         System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.");
         System.out.println("Are you ready? Write 'yes' if you are");
         Scanner scan = new Scanner (System.in);
-
         // check if the user wants to play
         String userInPut = scan.nextLine();
-//        String userChoice = scan.nextLine();
         String computerChoice = computerChoice();
-//        System.out.println("You Chose: " + userInPut);
-//        System.out.println("The computer chose: " + computerChoice);
         if(userInPut.equals("yes")){
             System.out.println("Great!");
             System.out.println("rock - paper - scissors, shoot!");
@@ -22,26 +17,12 @@ public class Game {
             System.out.println("The computer chose: " + computerChoice);
             String result = result(userChoice,computerChoice);
             System.out.println(result);
-
-
-
-
-            //if (userChoice == "Rock"){
-
             }
         else {
             System.out.println("Darn, some other time...!");
         }
 
         }
-//        else {
-//            System.exit(0);
-//        }
-//        System.out.println("\nYou chose:" + userChoice);
-//        System.out.println("The computer Chose" + computerChoice);
-//
-//        scan.close();
-//    }
     public static String computerChoice(){
         int choice = (int) (Math.random() * 3);
         System.out.println(choice);
@@ -54,46 +35,33 @@ public class Game {
     }
     public static String result(String userChoice, String computerChoice){
         String result = "";
-
         if (userChoice.equals("rock") && computerChoice.equals("scissors")){
             result = "you win";
-
         }
         else if(userChoice.equals("scissors") && computerChoice.equals("rock")){
             result = "you lose";
-
         }
         else if (userChoice.equals("paper") && computerChoice.equals("rock")){
             result = "you win";
-
         }
         else if (userChoice.equals("rock") && computerChoice.equals("paper")){
             result = "you lose";
-
         }
         else if (userChoice.equals("scissors") && computerChoice.equals("paper")){
             result = "you win";
-
         }
         else if (userChoice.equals("paper") && computerChoice.equals("scissors")){
             result = "you lose";
-
         }
         else if (userChoice.equals(computerChoice)){
             result = "you tie";
-
         }
         else {
             result = "Invalid choice";
         }
-
-
-
         return result;
-
     }
     public static void printResult(String userInPut, String computerChoice, String result){
-
 
 
     }
