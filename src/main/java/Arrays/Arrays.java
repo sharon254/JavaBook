a1cedc8b0823e33d10618d86e99bb19d41f25992
sharon254 <sharonknyabuti@gmail.com>
2023-01-11 12:48:00 +0300

    Arrays

---
+++ src/main/java/Arrays/Arrays.java
@@ -0,0 +1,75 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[]args){
+        /**
+         * An array let you store many values at once
+         * have to share same type
+         */
+        int[] marks = {76,99,87,77};
+        String[] names = {"Sharon", "Chloe", "Easton"};
+        System.out.println(marks[2]);
+        /**
+         * Looping arrays
+         */
+        String[] kingdoms = {
+                "Northern", "IceLand", "MainLand", "Wessex"
+        };
+        for(int i =0;i<kingdoms.length;i++){
+            System.out.println(i + "." +kingdoms[i]);
+        }
+        /**
+         *
+         */
+        String[] students = {"Mary", "Joe","Chloe", "Max"};
+        for(int i=0;i <students.length;i++){
+            System.out.println("Welcome to your exams " + students[i] + "Take seat " + (i+1));
+
+        }
+        String [] drinks = {"Tea", "Coffee", "Chocolate","Juice"};
+        System.out.println("What can I help you find?");
+        Scanner scan = new Scanner(System.in);
+        String choice = scan.nextLine();
+        System.out.println(choice);
+
+        for (int i=0;i<drinks.length;i++ ){
+            if(choice.equals(drinks[i])){
+                System.out.println("Welcome to table " + (i+1));
+            }
+
+        }
+//
+//        int max = mark[0];
+//        for (int i= 0;i <mark.length;i++){
+//            if(
+//                    mark[i]>max
+//            ){
+//                max = mark[i];
+//                System.out.println(max);
+//            }
+//
+//        }
+        /**
+         * 1.an array of the numbers
+         * 2.Starting with the fisrt one compare it with the 2nd
+         * 3.Let the holder hold the highest and compare that to the next
+         * 4.Loop through
+         */
+
+
+
+
+        // choice = drinks[i]
+        // print take seat number i
+
+
+
+
+
+
+
+    }
+
+}
